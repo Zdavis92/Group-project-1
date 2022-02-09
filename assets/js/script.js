6c6a00312fd56a202bc97937a91234a89e7af6a4
Zach Davis <zacharydavis92@gmail.com>
2022-02-08 23:30:16 -0600

    added weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+var lat = ""
+var lon = ""
+var cityName = ""
+var cityNameEl = document.querySelector("#currentCity");
+var checkWeatherBtn = document.querySelector("#checkWeather");
 var userNameInput = document.querySelector("#userName");
 var userWeightInput = document.querySelector("#userWeight");
 var userBodyfatInput = document.querySelector("#userBodyfat");
@@ -5,11 +10,20 @@ var userWeightGoalInput = document.querySelector("#weightGoal");
 var userBodyfatGoalInput = document.querySelector("#bodyfatGoal");
 var saveCurrentBtn = document.querySelector("#saveBtn");
 var saveGoalsBtn = document.querySelector("#saveGoals");
+var currentCon = {
+    icon: "",
+    temp: "",
+    wind: "",
+    humidity: "",
+    UVIndex: ""
+}
+
 var userCurrent = {
     name: "",
     weight: "",
     bodyfat: "",
 }
+
 var userGoals = {
     weightGoal: "",
     bodyfatGoal: "",
@@ -51,6 +65,61 @@ var loadUserCurrent = function() {
 var loadUserGoals = function() {
     userGoals = JSON.parse(localStorage.getItem("userGoals"));
 }
-saveCurrentBtn.addEventListener("click", saveUserCurrent);
 
-saveGoalsBtn.addEventListener("click", saveUserGoals);
+// function fetchs api to get lat and lon of the searched city and calls function to get the weather data
+var getCityLatLon = function(cityName) {
+    cityName = cityNameEl.value
+    if (cityName) {
+        var apiCity = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=7c0bd0cf3800dbf86808087317e3514f"
+        fetch(apiCity).then(function(response) {
+            response.json().then(function(data){
+                lat = data[0].lat
+                lon = data[0].lon
+                getWeatherData(lat, lon);
+            })
+        })
+    }
+    else {
+        alert("Enter a City Name")
+    }
+}
+// calls the api to weather data for current and forecast and saves the data in objects. Calls function to display the data
+var getWeatherData = function(lat, lon) {
+    var apiLatLon = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly,alerts&appid=7c0bd0cf3800dbf86808087317e3514f"
+    fetch(apiLatLon).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+            currentCon.icon = data.current.weather[0].icon
+            currentCon.temp = data.current.temp
+            currentCon.wind = data.current.wind_speed
+            currentCon.humidity = data.current.humidity
+            currentCon.UVIndex = data.current.uvi
+            displayWeather();
+        })
+    })
+}
+
+// function dynamicly creates elements and added weather data to them. Then displays them on the screen.
+var displayWeather = function() {
+    var currentCityEl = document.querySelector("#currentWeather");
+    var currentCityUvEl = document.createElement("p");
+    currentCityEl.innerHTML = ""
+    var currentCityNameEl = document.createElement("p");
+    var currentCityTempEl = document.createElement("p");
+    var currentCityWindEl = document.createElement("p");
+    var currentCityHumidityEl = document.createElement("p");
+    currentCityNameEl.textContent = cityName
+    currentCityTempEl.textContent = "Temp: " + currentCon.temp + "°F";
+    currentCityWindEl.textContent = "Wind: " + currentCon.wind + "MPH";
+    currentCityHumidityEl.textContent = "Humidity: " + currentCon.humidity + "%";
+    currentCityUvEl.textContent = "UV Index: " + currentCon.UVIndex;
+    currentCityEl.appendChild(currentCityNameEl)
+    currentCityEl.appendChild(currentCityTempEl)
+    currentCityEl.appendChild(currentCityWindEl)
+    currentCityEl.appendChild(currentCityHumidityEl)
+    currentCityEl.appendChild(currentCityUvEl)
+}
+
+// saveCurrentBtn.addEventListener("click", saveUserCurrent);
+checkWeatherBtn.addEventListener("click", getCityLatLon);
+// saveGoalsBtn.addEventListener("click", saveUserGoals);
